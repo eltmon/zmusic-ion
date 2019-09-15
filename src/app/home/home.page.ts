c8ded0c732f14367e5aa561e81ad8098cfc9d46d
Eltmon <eltmon@gmail.com>
2019-09-15 05:30:30 -0700

    Start

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,10 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private iab: InAppBrowser) {
+    //  const browser = this.iab.create('https://ionicframework.com/');
+    var ref = iab.create('https://www.zmusiccafe.com', '_self', 'location=yes');
+
+  }
 
 }
